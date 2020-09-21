04d09bf65e6a981b4e24f1530adbf3d0ec7aeaa5
seungchoi2019 <lioncsh200@gmail.com>
2020-09-21 14:41:32 -0500

    Title display

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+const schedule = {
+  title: "CS Courses for 2020-2021",
+};
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.bannerStyle}>{schedule.title}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#00f",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+  bannerStyle: {
+    color: "#888",
     fontSize: 32,
   },
 });
+
+export default App;
