23f842a3f31a51316191021139e308caaec6ec94
seungchoi2019 <lioncsh200@gmail.com>
2020-09-23 19:22:23 -0500

    Filtering

--- components/CourseList.js
+++ components/CourseList.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, ScrollView, View } from "react-native";
 import Course from "./Course";
+import TermSelector from "./TermSelector";
 
-const CourseList = ({ courses }) => (
-  <ScrollView>
-    <View style={styles.courseList}>
-      {courses.map((course) => (
+const termMap = { F: "Fall", W: "Winter", S: "Spring" };
+const terms = Object.values(termMap);
+
+const getCourseTerm = (course) => termMap[course.id.charAt(0)];
+
+const CourseList = ({ courses }) => {
+  const [selectedTerm, setSelectedTerm] = useState("Fall");
+
+  const termCourses = courses.filter(
+    (course) => selectedTerm === getCourseTerm(course)
+  );
+
+  return (
+    <ScrollView
+      style={styles.courseList}
+      contentContainerStyle={styles.container}
+    >
+      <TermSelector
+        selectedTerm={selectedTerm}
+        setSelectedTerm={setSelectedTerm}
+      />
+      {termCourses.map((course) => (
         <Course key={course.id} course={course} />
       ))}
-    </View>
-  </ScrollView>
-);
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
   courseList: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  container: {
     flex: 1,
     flexDirection: "row",
     flexWrap: "wrap",
